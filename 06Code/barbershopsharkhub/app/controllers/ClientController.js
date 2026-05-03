72b7a37fa31d6163721c9e2e11d8e1235304f595
Monge-Ricardo <ramonge1@espe.edu.ec>
2026-05-03 12:57:49 -0500

    add dashboards:

--- 06Code/barbershopsharkhub/app/controllers/ClientController.js
+++ 06Code/barbershopsharkhub/app/controllers/ClientController.js
@@ -1,16 +1,19 @@
-const path = require('path');
+const path = require('path')
 
-
-const viewsPath = path.join(__dirname, '../views/client');
+const viewsPath = path.join(__dirname, '../views/client')
 
 exports.dashboard = (req, res) => {
-    res.sendFile(path.join(viewsPath, 'dashboard.html'));
-};
+    res.sendFile(path.join(viewsPath, 'dashboard.html'))
+}
 
 exports.login = (req, res) => {
-    res.sendFile(path.join(viewsPath, 'auth/login.html'));
-};
+    res.sendFile(path.join(viewsPath, 'auth/login.html'))
+}
 
 exports.register = (req, res) => {
-    res.sendFile(path.join(viewsPath, 'auth/register.html'));
-};
+    res.sendFile(path.join(viewsPath, 'auth/register.html'))
+}
+
+exports.profile = (req, res) => {
+    res.sendFile(path.join(viewsPath, 'dashboard.html'))
+}
--- 06Code/barbershopsharkhub/app/controllers/ManagerController.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-exports.dashboard = (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/manager/dashboard.html'));
-};
