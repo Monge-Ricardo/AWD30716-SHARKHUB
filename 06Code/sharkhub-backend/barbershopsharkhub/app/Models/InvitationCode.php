a63a7f1e1ceb0647141152c2ca66192372ddd705
Monge-Ricardo <ramonge1@espe.edu.ec>
2026-06-23 22:06:01 -0500

    add backend

--- 06Code/sharkhub-backend/barbershopsharkhub/app/Models/InvitationCode.php
+++ 06Code/sharkhub-backend/barbershopsharkhub/app/Models/InvitationCode.php
@@ -4,35 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Availability extends Model
+class InvitationCode extends Model
 {
-    protected $table = 'availability';
-
+    protected $table = 'invitation_codes';
     protected $primaryKey = 'id';
-
     public $incrementing = false;
-
     protected $keyType = 'string';
-
     public $timestamps = false;
 
     protected $fillable = [
         'id',
         'barbershop_id',
-        'barber_id',
-        'day_of_week',
-        'start_time',
-        'end_time',
-        'is_available',
+        'code',
+        'expires_at',
+        'is_active',
+        'created_at',
     ];
 
     public function barbershop()
     {
         return $this->belongsTo(Barbershop::class, 'barbershop_id', 'id');
-    }
-
-    public function barber()
-    {
-        return $this->belongsTo(User::class, 'barber_id', 'id');
     }
 }
